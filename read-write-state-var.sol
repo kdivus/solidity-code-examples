b273e304b3e1024b506205d61ed087c49acc48a2
Krešimir Divić <kdivus@hotmail.com>
2021-02-15 14:31:54 +0100

    reading and writing to a state var

--- read-write-state-var.sol
+++ read-write-state-var.sol
@@ -1,2 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.7.4;
+
+contract SimpleStorage {
+    // state variable to store a number
+    uint256 public num;
+
+    // you need to send a transaction to write to a state variable
+    function set(uint256 _num) public {
+        num = _num;
+    }
+
+    // you can read from a state variable without sending a transaction
+    function get() public view returns (uint256) {
+        return num;
+    }
+}
