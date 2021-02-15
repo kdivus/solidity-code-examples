da5b419ecebe34bc7f1d9ad53f53ada832b2e1df
Krešimir Divić <kdivus@hotmail.com>
2021-02-15 14:28:28 +0100

    state vars

--- variables.sol
+++ variables.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.7.4;
 
 contract Variables {
-    // state variables are stored on the blockchain
+    // state variables are stored on the blockchain and declared outside a function
     string public text = "Hello";
     uint256 public num = 123;
 
     function doSomething() public {
-        // local variables are not stored to the blockchain
+        // local variables are declared inside a function and not stored to the blockchain and
         uint256 i = 234;
 
-        // global variables examples
+        // global variables examples - they provide information about the blockchain
         uint256 timestamp = block.timestamp; // current block timestamp
         address sender = msg.sender; // address of the caller
     }
